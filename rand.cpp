ff3d93a70c6e32f30e381f93d2b6741fac4116e8
svaze30 <vazeswaroop@gmail.com>
2024-02-10 13:55:30 +0530

    added new solutions

---
+++ rand.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+typedef long long ll;
+#define loop0(i, n) for (ll i = 0; i < n; i++)
+#define loop1(i, n) for (ll i = 1; i <= n; i++)
+
+#define mp make_pair;
+#define F first;
+#define S second;
+#define pb push_back;
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+using namespace std;
+
+void yesno(bool cond) {
+  if (cond)
+    yes;
+  else
+    no;
+}
+
+void debug(auto arr) {
+  for (auto i : arr)
+    cout << i << ' ';
+  cout << endl;
+}
+
+void solve();
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
+
+void solve() {
+  ll a;
+  cin >> a;
+  yesno(a < 0 or a % 3);
+}
